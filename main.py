9b76cd71e1b7430506e9b11fcc033bc62c2d3024
NEBULA1978 <rpascualey@gmail.com>
2023-06-05 13:50:11 +0200

    Creamos diccionario con python con clave valor con range la edad random

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 my_dict = {"Juan": {"edad": 30, "casado": True}}
 
 print(my_dict)#{'Juan': {'edad': 30, 'casado': True}}
@@ -9,3 +11,10 @@ age=[22,33,43,51]
 my_dict ={name[i]:age[i] for i in range(len(name))}
 
 print(my_dict)#{'Alex': 22, 'Anna': 33, 'Petter': 43, 'Angela': 51}
+
+# Random  edad
+my_dict ={name[i]:random.randint(10,50) for i in range(len(name))}
+print(my_dict)#{'Alex': 22, 'Anna': 33, 'Petter': 43, 'Angela': 51}
+
+
+
